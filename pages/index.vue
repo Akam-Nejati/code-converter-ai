7375c14f0fa9db6bfa5a104c1693e7b313a3e726
Akam-Nejti <mr.akam.320@gmail.com>
2023-05-04 18:22:44 +0330

    edit title text

--- pages/index.vue
+++ pages/index.vue
@@ -30,7 +30,7 @@ function convert() {
 
 <template>
   <div class="flex justify-center my-10 font-bold">
-    <h3 class="text-4xl">Code Converter</h3>
+    <h3 class="text-4xl">Code Converter Ai</h3>
   </div>
   <div>
     <div class="flex justify-between items-center lg:flex-row flex-col px-[1rem] sm:px-[5rem] md:px-[7rem] lg:px-0">
